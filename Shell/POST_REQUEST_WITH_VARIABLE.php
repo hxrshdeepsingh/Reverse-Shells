ffb8d9bc62c68780ffb8a59f29539af23f862bc3
Ishu <104856493+hxrshdeepsingh@users.noreply.github.com>
2025-11-17 14:52:52 +0530

    Update POST_REQUEST_WITH_VARIABLE.php

--- Shell/POST_REQUEST_WITH_VARIABLE.php
+++ Shell/POST_REQUEST_WITH_VARIABLE.php
@@ -1,3 +1,5 @@
-  $a = $_POST['system'];
-  $b = $_POST['cat /etc/password'];
-  $a($b);
+# Pseudocode: demonstrates insecure function call via POST input
+function_name = get_post_parameter("function");
+argument = get_post_parameter("argument");
+
+call_function( function_name, argument );
